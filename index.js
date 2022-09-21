27c452411416d5f5b054de730c4f2ca59f25e393
Moises Tadeo <moisestadeo10@gmail.com>
2022-09-20 21:05:01 -0700

    made a change in making the vcharacter move using function move()

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+function move(image){
+    image.style.position = 'fixed'
+    
+    function moveToCoordinates(left, bottom){
+        image.style.left = left + 'px'
+        image.style.bottom = bottom + 'px'
+    }
+
+    return {
+        to: moveToCoordinates
+    }
+}
+
+
+
 function newImage(url, left, bottom){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
+
+move(newImage('assets/green-character.gif')).to(100, 250)
+
+
+
 
 newImage('assets/green-character.gif', 100, 250)
 newImage('assets/tree.png', 200, 450)
